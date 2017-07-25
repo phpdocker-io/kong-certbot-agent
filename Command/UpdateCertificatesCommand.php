a1e2d7359c594b9453e48b3005bb5f9ab4635790
Luis Pabon <luis.pabon@auronconsulting.co.uk>
2017-07-25 15:03:25 +0100

    Remove unused import

--- Command/UpdateCertificatesCommand.php
+++ Command/UpdateCertificatesCommand.php
@@ -8,7 +8,6 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\VarDumper\VarDumper;
 
 /**
  * Handles requesting certificates for a given list of domains off Let's Encrypt via certbot's standalone method - it
