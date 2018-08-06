dd4637ace7977f637a90dd1967e9f4d50a91a79f
Deshi Xiao <xiaods@gmail.com>
2018-08-06 20:59:16 +0800

    support kong apikey mode
    Signed-off-by: Deshi Xiao <xiaods@gmail.com>

--- Command/UpdateCertificatesCommand.php
+++ Command/UpdateCertificatesCommand.php
@@ -48,6 +48,11 @@ class UpdateCertificatesCommand extends Command
                 InputArgument::REQUIRED,
                 'Comma separated list of domains to request certs for; eg: bar.com,foo.bar.com'
             )
+            ->addArgument(
+               'kong-apikey',
+               InputArgument::OPTIONAL,
+               'Kong Admin API key, eg: curl -X GET -H "apikey:xxxx"'
+            )
             ->addOption(
                 'test-cert',
                 't',
@@ -70,6 +75,7 @@ class UpdateCertificatesCommand extends Command
         $kongAdminUri = $input->getArgument('kong-endpoint');
         $domains      = $this->parseDomains($input->getArgument('domains'));
         $testCert     = $input->getOption('test-cert');
+        $kongApikey   = $input->getArgument('kong-apikey');
 
         // Compose cerbot command & execute
         $renewCmd = escapeshellcmd(sprintf(
@@ -91,6 +97,11 @@ class UpdateCertificatesCommand extends Command
 
         // Update kong admin with the new certificates foreach domain
         $guzzle = new \GuzzleHttp\Client();
+
+        // if kong api key is here,then add header
+        if ($kongApikey) {
+          $guzzle->setDefaultOption('headers/apiKey', $kongApikey);
+        }
 
         foreach ($domains as $domain) {
             $output->writeln(sprintf('Updating certificates config for %s', $domain));
