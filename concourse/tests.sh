0e0c3e1da8f1ba462cdd6a79c925819ad2364f62
Luis Pabon <luis.pabon@paddle.com>
2018-12-05 17:43:25 +0000

    m

--- concourse/tests.sh
+++ concourse/tests.sh
@@ -1,18 +1,28 @@
 #!/usr/bin/env bash
-apt-get update
-apt-get install -y php-xdebug
 
-mkdir /tmp/reports/
+# Ensure we exit with failure if anything here fails
+set -e
+
+# cd into the codebase, as per CI source
 cd code
 
+# Install xdebug & disable
+apt-get update
+apt-get install -y php-xdebug
+phpdismod xdebug
+
+# Store in here any test artifacts
+mkdir /tmp/reports/
+
+# Run unit tests
 composer -o install
 vendor/bin/phpunit
 
 # Placeholder for extracting coverage metric
 echo "fo" > /tmp/reports/phpunit
 
-ls -la vendor/bin
-vendor/bin/infection --threads=2 -s --configuration=infection.json.dist --min-msi=60 --min-covered-msi=95
+# Run mutation tests
+vendor/bin/infection --initial-tests-php-options="-d zend_extension=xdebug.so" --threads=2 -s --min-msi=60 --min-covered-msi=95
 
 # Placeholder for extracting coverage metric
 echo "fa" > /tmp/reports/infection
