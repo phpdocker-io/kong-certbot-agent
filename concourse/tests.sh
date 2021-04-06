c46c3ddf036356c6cd865346c30a9adf7e016328
Luis Pabon <luis.pabon@auronconsulting.co.uk>
2021-04-06 11:56:07 +0100

    Dependency updates

--- concourse/tests.sh
+++ concourse/tests.sh
@@ -9,18 +9,21 @@ INITIAL_FOLDER=`pwd`
 cd code
 mkdir reports
 
+PHP_VERSION=$(php -r "echo preg_replace('/.[0-9]+(-.*)?$/', '', phpversion());")
+echo "Detected PHP Version: ${PHP_VERSION}"
+
 # Install xdebug & disable
 apt-get update
-apt-get install -y php-xdebug
+apt-get install -y php${PHP_VERSION}-xdebug
 phpdismod xdebug
 
 composer -o install
 
 # Static analysis
-vendor/bin/phpstan -v analyse -l 7 src -c phpstan.neon  && printf "\n ${bold}PHPStan:${normal} static analysis good\n\n" || exit 1
+vendor/bin/phpstan -v analyse -l 7 src && printf "\n ${bold}PHPStan:${normal} static analysis good\n\n" || exit 1
 
 # Run unit tests
-php -d zend_extension=xdebug.so vendor/bin/phpunit --testdox
+XDEBUG_MODE=coverage php -d zend_extension=xdebug.so vendor/bin/phpunit --testdox
 
 # Run mutation tests
 vendor/bin/infection --coverage=reports/infection --threads=2 -s --min-msi=100 --min-covered-msi=100
